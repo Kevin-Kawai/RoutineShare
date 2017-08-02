98dac0d8d10b315f7b3a70a11c13aaa67a1d360e
Kevin Kawai <kevin.fredricks@gmail.com>
2017-08-02 01:23:48 +0000

    Changed authentication to be only new, had it on view as well

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,5 +1,5 @@
 class RoutinesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only:[:new,:create]
 
   def index
     @routines = Routine.all
