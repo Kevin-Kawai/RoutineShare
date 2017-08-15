f146ddbe4d6adfad14190857c29f70f716c8cfc0
Kevin Kawai <kevin.fredricks@gmail.com>
2017-08-15 08:47:32 +0000

    added test for account creation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe "User" do
+    it "should create a user" do
+      user = FactoryGirl.create(:user)
+      expect(user.email).to match(/dummyemail[0-9]*\@gmail\.com/)
+    end
+  end
 end
